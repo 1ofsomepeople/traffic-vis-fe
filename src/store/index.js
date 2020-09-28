1b4b479eb71cd06c3fda9d6e61945dc07470655f
dingchaofan <627844006@qq.com>
2020-09-28 22:30:58 +0800

    历史真实和预测目录，使用mobx

---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+import predictCompareStore from './predictCompare-store';
+
+export {
+    predictCompareStore,
+}
---
+++ src/store/predictCompare-store.js
@@ -0,0 +1,10 @@
+import {action, observable} from 'mobx';
+import {message} from 'antd';
+
+class PredictCompareStore {
+    // @observable historyGtDataList = [];
+}
+
+
+const predictCompareStore = new PredictCompareStore();
+export default predictCompareStore;
