6fb66d20b0aad14ba3e1eeaa0bf940428556ddc4
dingchaofan <627844006@qq.com>
2020-11-04 19:55:28 +0800

    build:将build构建文件加入git，为后续自动化部署做准备

---
+++ build/precache-manifest.a2a4d58472cdc46e9e8e5bd7b5670bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3667aa70b5d7767c390db48774f24d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de95973c1c007b8108b6",
+    "url": "./static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "d5877852d55931e29121",
+    "url": "./static/css/main.6195e378.chunk.css"
+  },
+  {
+    "revision": "de95973c1c007b8108b6",
+    "url": "./static/js/2.34f80b7a.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "./static/js/2.34f80b7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5877852d55931e29121",
+    "url": "./static/js/main.27f201d0.chunk.js"
+  },
+  {
+    "revision": "851fe923568fc0c36030",
+    "url": "./static/js/runtime-main.421e4984.js"
+  }
+]);
